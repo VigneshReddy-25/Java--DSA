67931449655f69b22dd22c51348232a78c8ab81a
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-04 19:08:13 +0530

    Add ArrayDeclaration class with example arrays

---
+++ DSA/Arrays/ArrayDeclaration.java
@@ -0,0 +1,15 @@
+package DSA.Arrays;
+
+public class ArrayDeclaration {
+    public static void main(String[] args) {
+        int arr1[];
+        int arr2[]=new int[5];
+        for(int i=0; i<arr2.length; i++){
+            System.out.println(arr2[i]);
+        }
+        int arr3[]={1,2,3,4,5};
+        for(int i=0; i<arr3.length; i++){
+            System.out.println(arr3[i]);
+        }
+    }
+}
