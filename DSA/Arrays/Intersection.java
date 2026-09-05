ef6e743c171e137ec50857b1194b803e13d8ef50
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-05 12:16:17 +0530

    Add Intersection class for array intersection
    Implement a program to find the intersection of two arrays.

---
+++ DSA/Arrays/Intersection.java
@@ -0,0 +1,24 @@
+package DSA.Arrays;
+
+public class Intersection {
+    public static void main(String args[]){
+      int a[]={1,2,2,4,6};
+      int b[]={2,2,3,4};
+      for(int i=0;i<a.length;i++){
+        boolean alreadyPrint=false;
+        for(int k=0;k<i;k++){
+          if(a[k]==a[i]){
+            alreadyPrint=true;
+            break;
+          }
+        }
+        if(alreadyPrint) continue;
+        for(int j=0;j<b.length;j++){
+          if(a[i]==b[j]){
+            System.out.print(a[i]+" ");
+            break;
+          }
+        }
+      }
+    }
+}
