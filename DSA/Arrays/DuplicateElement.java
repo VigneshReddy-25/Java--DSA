1e9fd944cabb3f3b4a65c8fcee6e3e75a45fc23d
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-05 12:14:14 +0530

    Add DuplicateElement class to find duplicates in array

---
+++ DSA/Arrays/DuplicateElement.java
@@ -0,0 +1,21 @@
+package DSA.Arrays;
+
+public class DuplicateElement {
+    public static void main(String[] args) {
+      int arr[]={1,3,5,6,7,8,1};
+      boolean found=false;
+      for(int i=0;i<arr.length;i++){
+        for(int j=i+1;j<arr.length;j++){
+          if(arr[i]==arr[j]){
+            System.out.println("duplicate: "+arr[i]);
+            found=true;
+            break;
+          }
+        }
+        if(found){
+          break;
+        }
+      }
+      if(!found) System.out.println("No Element");
+    }
+}
