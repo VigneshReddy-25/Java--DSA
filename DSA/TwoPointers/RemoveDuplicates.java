398d835f64a3fac5f08c4c62577308766a468e8e
Reddyvari Vignesh Reddy <reddyvignesh731@gmail.com>
2026-09-14 20:31:21 +0530

    Add RemoveDuplicates class to handle duplicate removal
    Implemented a method to remove duplicates from a sorted array using the two-pointer technique.

---
+++ DSA/TwoPointers/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package DSA.TwoPointers;
+
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String at[]){
+        int arr[]={1,2,3,3,4,4,5,6};
+        int slow=0;
+        int cnt=0;
+        for(int fast=0;fast<arr.length;fast++){
+            if(arr[slow]!=arr[fast]){
+                slow++;
+                arr[slow]=arr[fast];
+                cnt++;
+            }
+        }for(int i=0;i<cnt;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+           
